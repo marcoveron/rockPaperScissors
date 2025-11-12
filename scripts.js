d521f6098398e92ef75a2d3ddcd80f7f0ed3c436
Marco Verón <mv.marcoveron@gmail.com>
2025-11-12 14:21:05 +0800

    Add a prompt for the user
    A prompt is added for the user to select between (0,2) (rock, paper,
    scissors) with the prompt() function.

--- scripts.js
+++ scripts.js
@@ -2,4 +2,7 @@ function getComputerNumber(max = 3){
     return Math.floor(Math.random() * max);
 }
 
+const computerValue = getComputerNumber();
+
+const humanValue = prompt("Please enter '0' for ROCK, '1' for PAPER and '2' for SCISSORS");
 
