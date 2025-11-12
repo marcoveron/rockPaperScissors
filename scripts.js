da31618a0c550c3da3211c8e43027170a2c37e07
Marco Verón <mv.marcoveron@gmail.com>
2025-11-12 14:23:25 +0800

    Create variables for scores
    Two variables are created computerScore and humanScore to keep track of
    the scores (variables are initialized in 0).

--- scripts.js
+++ scripts.js
@@ -6,3 +6,7 @@ const computerValue = getComputerNumber();
 
 const humanValue = prompt("Please enter '0' for ROCK, '1' for PAPER and '2' for SCISSORS");
 
+
+let humanScore = 0;
+
+let computerScore = 0;
