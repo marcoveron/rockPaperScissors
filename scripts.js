8f4656913084d8fd0018e935914c87314952ff24
Marco Verón <mv.marcoveron@gmail.com>
2025-11-13 22:20:23 +0800

    Add game function
    A function to play the game was added, inside this function there is the
    play round function that it's called 5 times using a for loop. Inside
    the play round functions the getValue() functions are called to get each
    play choice per round, and after this the winner of the round is
    evaluated and gets it's score updated (+1).

--- scripts.js
+++ scripts.js
@@ -1,12 +1,66 @@
-function getComputerNumber(max = 3){
-    return Math.floor(Math.random() * max);
+let humanScore = 0;
+let computerScore = 0;
+let ties = 0;
+
+function getComputerNumber(max = 3){ 
+    let number = Math.floor(Math.random() * max);
+    if (number == 0){
+        return "rock";
+    } else if (number == 1){
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
-const computerValue = getComputerNumber();
+function getHumanValue(){
+    return prompt("Please enter your choice: 'ROCK', 'PAPER' or 'SCISSORS'").toLowerCase();
+}
 
-const humanValue = prompt("Please enter '0' for ROCK, '1' for PAPER and '2' for SCISSORS");
+function playGame(computer, human){
 
+    function playRound(computer, human){
 
-let humanScore = 0;
+        const computerValue = getComputerNumber();
+        const humanValue = getHumanValue();
 
-let computerScore = 0;
+        if(computerValue === humanValue){
+            console.log(`You tie! ${computerValue} ties with ${humanValue}.`);
+            ties += 1;
+        } else if(computerValue == "rock" && humanValue == "paper") {
+            console.log(`You win! ${computerValue} losses to ${humanValue}.`);
+            humanScore += 1;
+        } else if(computerValue == "rock" && humanValue == "scissors") {
+            console.log(`You lose! ${computerValue} wins ${humanValue}.`);
+            computerScore += 1;
+        } else if(computerValue == "paper" && humanValue == "rock") {
+            console.log(`You lose! ${computerValue} wins ${humanValue}.`);
+            computerScore += 1;
+        } else if(computerValue == "paper" && humanValue == "scissors") {
+            console.log(`You win! ${computerValue} losses to ${humanValue}.`);
+            humanScore += 1;
+        } else if(computerValue == "scissors" && humanValue == "rock") {
+            console.log(`You win! ${computerValue} losses to ${humanValue}.`);
+            humanScore += 1;
+        } else if(computerValue == "scissors" && humanValue == "paper") {
+            console.log(`You lose! ${computerValue} wins ${humanValue}.`);
+            computerScore += 1;
+        }
+    }
+
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+}
+
+playGame();
+
+if (computerScore === humanScore){
+    console.log(`\nIt's a TIE, both players have ${computerScore} point(s)!`)
+} else if (computerScore > humanScore){
+    console.log(`\nYou lose! :( the computer score is ${computerScore} and yours is ${humanScore}.`)
+} else {
+    console.log(`\nYou win! :) the computer score is ${computerScore} and yours is ${humanScore}.`)
+}
