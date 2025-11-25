86e0943bb9730873cef3055b0fa9f4e66cdf951a
Marco Verón <mv.marcoveron@gmail.com>
2025-11-25 12:47:34 +0800

    Add interface
    three buttons are added for the player to choose between rock paper or
    scissors. playRound() is called with the player current selection and
    the round is played. a div element is keep track of the current scores
    and display them.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,17 @@ let humanScore = 0;
 let computerScore = 0;
 let ties = 0;
 
+let display = document.querySelector(".gameDisplay");
+let secondDisplay = document.querySelector(".roundDisplay");
+
+let rockHuman = document.querySelector('.rockHuman');
+let paperHuman = document.querySelector('.paperHuman');
+let scissorsHuman = document.querySelector('.scissorsHuman');
+
+rockHuman.addEventListener("click", () => playRound(undefined,"rock"));
+paperHuman.addEventListener("click", () => playRound(undefined,"paper"));
+scissorsHuman.addEventListener("click", () => playRound(undefined,"scissors"));
+
 function getComputerNumber(max = 3){ 
     let number = Math.floor(Math.random() * max);
     if (number == 0){
@@ -13,54 +24,37 @@ function getComputerNumber(max = 3){
     }
 }
 
-function getHumanValue(){
-    return prompt("Please enter your choice: 'ROCK', 'PAPER' or 'SCISSORS'").toLowerCase();
-}
+function playRound(computer, human){
 
-function playGame(computer, human){
+    const computerValue = getComputerNumber();
+    const humanValue = human;
 
-    function playRound(computer, human){
-
-        const computerValue = getComputerNumber();
-        const humanValue = getHumanValue();
-
-        if(computerValue === humanValue){
-            console.log(`You tie! ${computerValue} ties with ${humanValue}.`);
-            ties += 1;
-        } else if(computerValue == "rock" && humanValue == "paper") {
-            console.log(`You win! ${computerValue} losses to ${humanValue}.`);
-            humanScore += 1;
-        } else if(computerValue == "rock" && humanValue == "scissors") {
-            console.log(`You lose! ${computerValue} wins ${humanValue}.`);
-            computerScore += 1;
-        } else if(computerValue == "paper" && humanValue == "rock") {
-            console.log(`You lose! ${computerValue} wins ${humanValue}.`);
-            computerScore += 1;
-        } else if(computerValue == "paper" && humanValue == "scissors") {
-            console.log(`You win! ${computerValue} losses to ${humanValue}.`);
-            humanScore += 1;
-        } else if(computerValue == "scissors" && humanValue == "rock") {
-            console.log(`You win! ${computerValue} losses to ${humanValue}.`);
-            humanScore += 1;
-        } else if(computerValue == "scissors" && humanValue == "paper") {
-            console.log(`You lose! ${computerValue} wins ${humanValue}.`);
-            computerScore += 1;
-        }
+    if(computerValue === humanValue){
+        display.textContent = `You tie! ${computerValue} ties with ${humanValue}.`;
+        ties += 1;
+    } else if(computerValue == "rock" && humanValue == "paper") {
+        display.textContent = `You win! ${computerValue} losses to ${humanValue}.`;
+        humanScore += 1;
+    } else if(computerValue == "rock" && humanValue == "scissors") {
+        display.textContent = `You lose! ${computerValue} wins ${humanValue}.`;
+        computerScore += 1;
+    } else if(computerValue == "paper" && humanValue == "rock") {
+        display.textContent = `You lose! ${computerValue} wins ${humanValue}.`;
+        computerScore += 1;
+    } else if(computerValue == "paper" && humanValue == "scissors") {
+        display.textContent = `You win! ${computerValue} losses to ${humanValue}.`;
+        humanScore += 1;
+    } else if(computerValue == "scissors" && humanValue == "rock") {
+        display.textContent = `You win! ${computerValue} losses to ${humanValue}.`;
+        humanScore += 1;
+    } else if(computerValue == "scissors" && humanValue == "paper") {
+        display.textContent = `You lose! ${computerValue} wins ${humanValue}.`;
+        computerScore += 1;
     }
 
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
+    secondDisplay.textContent = `computer: ${computerScore} you: ${humanScore}`
+
 }
 
-playGame();
 
-if (computerScore === humanScore){
-    console.log(`\nIt's a TIE, both players have ${computerScore} point(s)!`)
-} else if (computerScore > humanScore){
-    console.log(`\nYou lose! :( the computer score is ${computerScore} and yours is ${humanScore}.`)
-} else {
-    console.log(`\nYou win! :) the computer score is ${computerScore} and yours is ${humanScore}.`)
-}
+
